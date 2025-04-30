5d8b2217c8e86d359a7ca830441ee14888ca807e
Subhrajyoti <subha.ionic3@gmail.com>
2025-05-01 04:19:02 +0530

    typo fix - app crash

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -5,7 +5,7 @@ const movieRoutes = require("../../src/routes/movie");
 
 const api = express();
 
-app.get("/", (req, res) => res.send("Hello I am listening"));
+api.get("/", (req, res) => res.send("Hello I am listening"));
 
 api.use("/api/", router);
 api.use("/api/", movieRoutes);
