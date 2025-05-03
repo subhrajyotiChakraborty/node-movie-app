41f35340165bc07e9e4675c8559d899a24419ca5
Subhrajyoti <subha.ionic3@gmail.com>
2025-05-03 20:49:09 +0530

    Updated the route to get movie details and all movies by name

--- src/controller/movie.js
+++ src/controller/movie.js
@@ -1,7 +1,20 @@
-exports.getMovie = async (req, res, next) => {
+exports.getAllMoviesByName = async (req, res, next) => {
   const { movieName } = req.params;
+  const { page } = req.query;
   const promiseResponse = await fetch(
-    `https://omdbapi.com/?apikey=${process.env.API_KEY}&s=${movieName}`,
+    `https://omdbapi.com/?apikey=${process.env.API_KEY}&s=${movieName}&page=${page}`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await promiseResponse.json();
+  res.status(200).send(data);
+};
+
+exports.getMovieDetails = async (req, res, next) => {
+  const { movieId } = req.params;
+  const promiseResponse = await fetch(
+    `https://omdbapi.com/?apikey=${process.env.API_KEY}&i=${movieId}`,
     {
       method: "GET",
     }
--- src/routes/movie.js
+++ src/routes/movie.js
@@ -4,6 +4,7 @@ const movieController = require("../controller/movie");
 
 const router = express.Router();
 
-router.get("/movies/:movieName", movieController.getMovie);
+router.get("/movies/:movieName", movieController.getAllMoviesByName);
+router.get("/movie/:movieId", movieController.getMovieDetails);
 
 module.exports = router;
