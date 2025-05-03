6db3379b39219500a7eb0ed013c2235344840e74
Subhrajyoti <subha.ionic3@gmail.com>
2025-05-03 21:12:54 +0530

    enabling the CORS

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const cors = require("cors");
 
 const movieRoutes = require("../../src/routes/movie");
 
 const api = express();
 
 api.get("/", (req, res) => res.send("Hello I am listening"));
-
+api.use(cors());
 api.use("/api/", movieRoutes);
 
 export const handler = serverless(api);
