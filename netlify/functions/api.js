0a24da4b82df584aff696ef4951c14dcc591755a
Subhrajyoti <subha.ionic3@gmail.com>
2025-05-04 18:32:30 +0530

    revert changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -4,34 +4,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const movieRoutes = require("../../src/routes/movie");
-const { mongoConnect } = require("../../src/util/database");
+// const { mongoConnect } = require("../../src/util/database");
 
 const api = express();
 
+api.get("/", (req, res) => res.send("Hello I am listening"));
 api.use(cors());
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true }));
+api.use("/api/", movieRoutes);
 
-// This middleware will run on every request and ensure DB is available
-api.use(async (req, res, next) => {
-  try {
-    mongoConnect(() => {
-      next();
-    }); // Attach cached DB connection to request
-  } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
-    res.status(500).json({ error: "Failed to connect to database" });
-  }
-});
+// mongoConnect
 
-// Define routes
-api.get("/", (req, res) => res.send("Hello I am listening"));
-api.use("/api", movieRoutes);
-
-// Wrap Express app with serverless-http
-const handler = serverless(api);
-
-// Proper export
-exports.handler = async (event, context) => {
-  return await handler(event, context);
-};
+export const handler = serverless(api);
